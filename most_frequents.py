796bd563aacf158bbf41789230cfffc66694b2e1
Yogendra Singh <90251984+Yogendra1213@users.noreply.github.com>
2021-09-28 21:00:43 +0530

    Add files via upload

---
+++ most_frequents.py
@@ -0,0 +1,18 @@
+st="Mississippi"
+def most_frequent(stri):
+    i=0
+    s=0
+    m=0
+    p=0
+    for j in st:
+        if j=="i" or j=="I":
+            i+=1
+        if j=="s" or j=="S":
+            s+=1
+        if j=="m" or j=="M":
+            m+=1
+        if j=="p" or j=="P":
+            p+=1
+    print("i=",i,"s=",s,"m=",m,"p=",p)
+
+most_frequent(st)
